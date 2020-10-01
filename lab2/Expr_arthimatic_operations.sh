8b599518a01b7c1d57fefa15756ef5695d685899
shrav2001 <53821693+shrav2001@users.noreply.github.com>
2020-10-01 15:15:52 +0530

    Update Expr_arthimatic_operations.sh

--- lab2/Expr_arthimatic_operations.sh
+++ lab2/Expr_arthimatic_operations.sh
@@ -7,8 +7,10 @@ resulta=`expr $a + $b`
 resultb=`expr $a - $b`
 resultc=`expr $a \* $b`
 resultd=`expr $a / $b`
+resulte=`expr $a % $b`
 
 echo "Addition result: $resulta"
 echo "Subtraction result: $resultb"
 echo "Multiplication result: $resultc"
-echo "Division result: $resultd"
+echo "Division result (quotient) : $resultd"
+echo "Modular Division result : $resulte"
