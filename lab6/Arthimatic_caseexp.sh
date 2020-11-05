efd7e3cfe4b06f9dcc4f508c75189259fee21bd5
shrav2001 <53821693+shrav2001@users.noreply.github.com>
2020-11-05 14:56:24 +0530

    Update Arthimatic_caseexp.sh

--- lab6/Arthimatic_caseexp.sh
+++ lab6/Arthimatic_caseexp.sh
@@ -1,3 +1,33 @@
 #!/bin/sh
 #Arithmetic Operations on a set of two numbers using case expression
 
+echo "Enter first no"
+read a
+
+echo "Enter second number"
+read b
+
+echo "1. Addition\n2.Subtraction\n3.Multiplication\n4.Division"
+
+echo "Enter your choice"
+read option
+
+case $option in
+	
+	1) sum=`expr $a + $b`
+	   echo "Sum:  "$sum
+	   ;;
+	2) sub=`expr $a - $b`
+	   echo "Difference:  "$sub
+	   ;;
+	3) product=`expr $a \* $b`
+	   echo "Product:   "$product
+	   ;;
+	4) div=`expr $a / $b`
+	   echo "Quotient:   "$div
+	   ;;
+	*) echo "Not a valid option"
+	   ;;
+esac 
+
+
