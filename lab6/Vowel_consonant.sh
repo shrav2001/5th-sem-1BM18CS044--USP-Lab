dde830b5f89b6d94a24a94b31e805ba104a121c6
shrav2001 <53821693+shrav2001@users.noreply.github.com>
2020-11-05 15:00:02 +0530

    Update Vowel_consonant.sh

--- lab6/Vowel_consonant.sh
+++ lab6/Vowel_consonant.sh
@@ -1,2 +1,27 @@
 #!/bin/sh
 #check whether the entered character is Vowel or consonant.
+
+echo "Enter the alphabet"
+read char
+
+case $char in
+
+	'a') echo "Vowel"
+	;;
+
+	'e') echo "Vowel"
+	;;
+
+	'i') echo "Vowel"
+	;;
+
+	'o') echo "Vowel"
+	;;
+
+	'u') echo "Vowel"
+	;;
+
+	*) echo "Consonant"
+	;;
+
+esac
