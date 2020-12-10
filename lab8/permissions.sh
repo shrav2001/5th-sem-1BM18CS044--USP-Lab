5b8b347a48aa441d0dedc11d10796a801d540da3
shrav2001 <53821693+shrav2001@users.noreply.github.com>
2020-12-10 15:09:38 +0530

    Create permissions.sh

---
+++ lab8/permissions.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+if [ $# -ne 2 ]
+then
+echo "No arguments"
+elif [ ! -e $1 -o ! -e $2 ]
+then
+echo "FIle does not exist"
+else
+p1=`ls -l $1|cut -c 2-10`
+p2=`ls -l $2|cut -c 2-10`
+if [ $p1 = $p2 ]
+then
+echo "File permissions are equal & is $p1"
+else
+echo "File permissions are not equal"
+echo "1st file $p1"
+echo "2nd file $p2"
+fi
+fi
